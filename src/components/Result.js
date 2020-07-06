595fee295d436a19479961542f838e0b1bd7fb81
ashley003 <ashleyfromedmonds@gmail.com>
2020-07-06 12:51:29 -0700

    External links work for https websites

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const Result = (props) => {
+    const fullLink = props.link
+    const link = `https://${fullLink.substring(7, fullLink.length - 1)}`
 
     return (
         <div>
-            <h3>Title: {props.title}</h3>
+            <a rel={'external'} target="_blank" href={link}>Title: {props.title} </a>
             <h3>Link: {props.link}</h3>
             <h3>Snippet: {props.snippet}</h3>
         </div>
