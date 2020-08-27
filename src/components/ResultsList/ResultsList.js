0191045fe4d0f94f6015af7260390d9544d1d795
ashley003 <ashleyfromedmonds@gmail.com>
2020-08-27 16:26:25 -0700

    Fix className warning

--- src/components/ResultsList/ResultsList.js
+++ src/components/ResultsList/ResultsList.js
@@ -4,7 +4,7 @@ import Result from '../Result/Result';
 const ResultsList = (props) => {
 
     return (
-        <div class="flex flex-column">
+        <div className="flex flex-column">
             {
                 props.info.map((item, i) => {
                     return (<Result key={i} title={item.title} link={item.link} snippet={item.snippet}/>);
